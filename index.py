c1f05fd16ff2bf6c8ef20d63f41f48ab4ac560e0
Mr.Person <Mr.Person@person.com>
2023-07-24 08:12:45 -0300

    added

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
-from robolink import *  # Importa el módulo de conexión con RoboDK
+from robolink import Robolink  # Importa el módulo de conexión con RoboDK
 from robodk import *    # Importa la librería de funciones matemáticas
 
 # Inicializa la conexión con RoboDK
-RDK = robolink()
+RDK = Robolink()
 
 # Obtiene el brazo robótico en el espacio de trabajo (asegúrate de que el nombre coincida)
 robot = RDK.Item('Nombre_del_Brazo_Robotico')
@@ -15,7 +15,7 @@ pos_final = [120, 45, 90]  # [anguloHombro, anguloCodo, anguloPinza]
 velocidad = 30
 
 # Mueve el brazo robótico a la posición inicial
-robot.MoveJ(pos_inicial, vel=velocidad)
+robot.MoveJ(pos_inicial, velocidad)
 
 # Mueve el brazo robótico a la posición final
-robot.MoveJ(pos_final, vel=velocidad)
+# robot.MoveJ(pos_final, velocidad)
